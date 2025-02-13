0939c65bdf01c4176d99bf8e5a72f250a0c95d75
Nir Izhak <99140511+NirIzhak@users.noreply.github.com>
2025-02-13 16:49:44 +0200

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,6 +1,6 @@
 const overlay = document.createElement('div');
 overlay.id = 'loader';
-overlay.style.cssText = 
+overlay.style.cssText = `
     display: flex;
     justify-content: center;
     align-items: center;
@@ -11,20 +11,20 @@ overlay.style.cssText =
     height: 100%;
     background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
-;
+`;
 
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
-lottieContainer.style.cssText = 
-    width: 64px;
-    height: 64px;
+lottieContainer.style.cssText = `
+    max-width: 100%;
+    max-height: 100%;
     display: none;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    overflow: hidden;
-;
+`;
+lottieContainer.style.display = 'none';
 
 document.body.appendChild(overlay);
 document.body.appendChild(lottieContainer);
@@ -32,17 +32,18 @@ document.body.appendChild(lottieContainer);
 function hideOverlay() {
     overlay.style.display = 'none';
     lottieContainer.style.display = 'block';
-
+    // Substitua pela URL direta do seu arquivo JSON do Lottie.
     lottie.loadAnimation({
         container: lottieContainer,
-        renderer: 'svg', // 'canvas' if you prefer
+        renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://NirIzhak.github.io/preloaderJSLottie/JVGIsylS3F-3.json',
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid meet' // Keeps animation correctly sized
-        }
+        path: 'https://jvsc99.github.io/preloaderJSLottie/JVGIsylS3F-3.json',
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    hideOverlay(); // Teste sem o setTimeout
+});
 
 window.addEventListener('load', hideOverlay);
